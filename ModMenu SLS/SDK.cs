3af27b8a2ac7e106161473f084a91f707789b211
Zordon1337 <zordon@zndev.xyz>
2023-11-28 22:01:04 +0100

    New UI

---
+++ ModMenu SLS/SDK.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ModMenu_SLS
+{
+    public static class SDK
+    {
+        public static Texture2D MakeTexture(int width, int height, Color col)
+        {
+            Color[] pix = new Color[width * height];
+            for (int i = 0; i < pix.Length; ++i)
+            {
+                pix[i] = col;
+            }
+
+            Texture2D result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
+        }
+        
+    }
+}
--- ModMenu SLS/Shit.cs
+++ ModMenu SLS/Shit.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using MelonLoader;
+using System.Reflection.Emit;
 
 namespace ModMenu_SLS
 {
@@ -8,8 +9,11 @@ namespace ModMenu_SLS
     {
         bool bNoNeeds = false;
         TimeManager Time;
-        
-
+        private GUISkin windowstyle;
+        float wallettoadd = 0;
+        float banktoadd = 0;
+        float cardtoadd = 0;
+        static int page = 0;
         public IEnumerator Optimization()
         {
             for (; ; )
@@ -18,12 +22,76 @@ namespace ModMenu_SLS
                 yield return new WaitForSeconds(1f);
             }
         }
-        float wallettoadd = 0;
-        float banktoadd = 0;
-        float cardtoadd = 0;
+        void RenderUI(int id)
+        {
+
+            GUI.backgroundColor = Color.red;
+            GUI.color = Color.green;
+            
+            // render navbar
+            if (GUI.Button(new Rect(0, 15, 175, 50), "Teleporter"))
+            {
+                page = 0;
+            }
+            if (GUI.Button(new Rect(175, 15, 175, 50), "Player Stats"))
+            {
+                page = 1;
+            }
+            GUI.BeginGroup(new Rect(0, 50, 350, 400));
+            if(page == 0)
+            {
+                GUILayout.Label("Not implemented yet");
+            } else
+            {
+                if (GUILayout.Button("Disable Needs:" + bNoNeeds))
+                {
+                    bNoNeeds = !bNoNeeds;
+                }
+
+                GUILayout.Space(10f);
+
+                GUILayout.Label("Money Amount to add to card");
+                cardtoadd = float.Parse(GUILayout.TextField(cardtoadd.ToString()));
+                if (GUILayout.Button("Add Money to Card"))
+                {
+                    PlayerPrefs.SetFloat("cardmoney", PlayerPrefs.GetFloat("cardmoney") + cardtoadd);
+                }
+
+                GUILayout.Space(10f);
+
+                GUILayout.Label("Money Amount to add to wallet");
+                wallettoadd = float.Parse(GUILayout.TextField(wallettoadd.ToString()));
+                if (GUILayout.Button("Add Money to wallet"))
+                {
+                    PlayerPrefs.SetFloat("money", PlayerPrefs.GetFloat("money") + wallettoadd);
+                }
+
+                GUILayout.Space(10f);
+
+                GUILayout.Label("Money Amount to add to bank");
+                banktoadd = float.Parse(GUILayout.TextField(banktoadd.ToString()));
+                if (GUILayout.Button("Add Money to bank"))
+                {
+                    PlayerPrefs.SetFloat("bankmoney", PlayerPrefs.GetFloat("bankmoney") + banktoadd);
+                }
+                if(GUILayout.Button("Set Night"))
+                {
+                    Time.time = 4f;
+                }
+                if (GUILayout.Button("Set Day"))
+                {
+                    Time.time = 12f;
+                }
+            }
+            GUI.EndGroup();
+        }
         public override void OnGUI()
         {
-            GUI.backgroundColor = Color.white;
+            
+            
+            var window = GUI.Window(0, new Rect(Screen.width / 2 - 175, Screen.height / 2 - 250, 350, 500),RenderUI,"SLS mod menu");
+            
+            /*GUI.backgroundColor = Color.white;
             GUI.contentColor = Color.red;
             if (GUILayout.Button("Disable Needs:" + bNoNeeds))
             {
@@ -75,6 +143,7 @@ namespace ModMenu_SLS
         int calls = 0;
         public override void OnUpdate()
         {
+            
             calls++;
             if(calls > 125)
             {
